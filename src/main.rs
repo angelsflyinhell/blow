25d6a01935a6dcf8478aac6fc66b855932ac7cd1
angelsflyinhell <azami@namespace.media>
2022-11-30 23:49:09 +0100

    Yay things

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, guard};
+use actix_session::{storage::RedisActorSessionStore, SessionMiddleware, Session};
+use actix_web::{
+    get, post,
+    web::{self},
+    App, HttpResponse, HttpServer, Responder,
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let private_key = actix_web::cookie::Key::generate();
+
+    HttpServer::new(move || {
         App::new()
-        .service(hello)
-        .service(echo)
-        .service(
-            web::scope("/")
-            .guard(guard::Header("cross-origin-things", "value"))
-        )
-        .route("/hey", web::get().to(manual_hello))
+            .wrap(
+                SessionMiddleware::builder(
+                    RedisActorSessionStore::new("qwq.sh:6379"),
+                    private_key.clone(),
+                )
+                .build(),
+            )
+            .service(hello)
+            .service(echo)
     })
-    .bind("localhost:8080")?
+    .bind(("127.0.0.1", 8080))
+    .expect("Port is already in use. (Didn't happen in testing, don't ask me why.)")
     .run()
     .await
 }
 
-#[get("/")]
-async fn hello() -> impl Responder {
+#[get("/field/{id}")] // somehow doesn't work yet.
+async fn hello(path: web::Path<(u32,)>, session: Session) -> impl Responder {
+    if let Err(e) = session.insert("test_key", 42) {
+        eprintln!("Could not insert data into database: {}", e);
+        return HttpResponse::InternalServerError().body("Could not insert data into database.");
+    }
+
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
+#[get("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
 }
